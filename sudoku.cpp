667dec89cb81a26b50938d6b00376a72fb81cf20
Aeron panta <078bct004@student.ioepc.edu.np>
2024-03-11 00:14:59 +0545

    day 2

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,11 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class condition
+class condition1
 {
+    bool check_row()
+    { int arr[9][9];
+       int key,row_position;
+       for (int j=0;j<9;j++)
+       {
+            if(arr[row_position][j]!=key)
+            {
+                return true;
+            }
 
+        }
+        return false;
+       
+    }
+    bool check_column()
+    {
+        int arr[9][9];
+       int key,col_position;
+       for (int i=0;i<9;i++)
+       {
+            if(arr[i][col_position]!=key)
+            {
+                return true;
+            }
+       }
+       return false;
+    }
+    bool check_matrix()
+    { 
+        int arr[9][9];
+        int row,col,key;
+        int row_start=row-row%3;
+        int col_start=col-col%3;
+      for(int i=1;i<=9;i++)
+      {
+        if(arr[3*(row/3)+i/3][3*(col/3)+i/3]==key)
+        {
+            return false;
+          
+            
+        }
+      
+      }
+      return true;
+
+    }
 };
 int main()
-{cout <<"hello world";
-
-
+{
+    condition1 c1;
+    return 0;
 }
